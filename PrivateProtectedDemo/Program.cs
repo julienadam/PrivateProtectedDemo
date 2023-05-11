f3fd4c498efab77dd8649fef8c0f78166514936a
Julien Adam <julien.adam@arcodia.fr>
2023-05-11 10:29:33 +0200

    Simple demo for private protected members

---
+++ PrivateProtectedDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using PrivateProtectedLib;
+
+Console.WriteLine("Hello, World!");
+
+class DerivedClass2 : BaseClass
+{
+    void Access()
+    {
+        // CS0122 car on est pas dans la même assembly
+        someField = 42;
+    }
+}
---
+++ PrivateProtectedLib/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿namespace PrivateProtectedLib;
+public class BaseClass
+{
+    private protected int someField = 42;
+}
+
+public class DerivedClass : BaseClass
+{
+	public void SomeMethod()
+    {
+        // Ok
+        base.someField = 12;
+    }
+}
+
+public class SomeOtherClass
+{
+    public void SomeMethod()
+    {
+        BaseClass b = new BaseClass();
+
+        // Ne compile pas.
+        Console.WriteLine(b.someField);
+    }
+}
